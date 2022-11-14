79ec2f40d2df8542d9ca2298ff9ad514ed485b9a
Kyleroi Sambat <kyleroisambat@Kylerois-MacBook-Air.local>
2022-11-13 17:04:39 -0700

    final draft

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-(function() {
+/*(function() {
   'use strict';
 
   // YOUR CODE HERE
 })();
+*/ // dont use outdated IIFE or something
+
+
+
+let stopButton = document.querySelector('#stopButton')
+let stopLight = document.querySelector('#stopLight')
+
+let slowButton = document.querySelector('#slowButton')
+let slowLight = document.querySelector('#slowLight')
+
+
+let goButton = document.querySelector('#goButton')
+let goLight = document.querySelector('#goLight')
+
+stopButton.addEventListener('click', function() {
+  stopLight.classList.toggle('stop')
+  
+})
+
+slowButton.addEventListener('click', function() {
+ slowLight.classList.toggle('slow')
+
+})
+
+goButton.addEventListener('click', function() {
+  goLight.classList.toggle('go')
+  
+})
+
+
+function addEventListener(button) {
+  button.addEventListener('mouseenter', function() {
+    console.log(`Entered ${button.textContent} button`)
+  })
+
+  button.addEventListener('mouseleave', function() {
+    console.log(`Left ${button.textContent} button`)
+  })
+}
+
+
+
+addEventListener(stopButton)
+addEventListener(slowButton)
+addEventListener(goButton)
+
+
+
+
+
+
+
